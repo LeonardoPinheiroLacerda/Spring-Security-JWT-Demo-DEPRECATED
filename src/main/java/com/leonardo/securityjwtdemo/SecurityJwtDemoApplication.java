1a5820ed76b6b156bc9e89eff83c5cdc3097d467
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-03-26 14:25:59 -0300

    Projeto criado

---
+++ src/main/java/com/leonardo/securityjwtdemo/SecurityJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.securityjwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtDemoApplication.class, args);
+	}
+
+}
